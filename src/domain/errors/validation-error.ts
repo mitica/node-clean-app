410fe9504b0df505c2cc46b1981e36e8b4ab47f4
Mitica <canteadumitru@gmail.com>
2019-07-30 14:58:05 +0300

    no eslint

--- src/domain/errors/validation-error.ts
+++ src/domain/errors/validation-error.ts
@@ -1,23 +1,29 @@
 import { BaseError } from "./error";
 
 export class ValidationError extends BaseError {
+  static get CODE() {
+    return "E2000";
+  }
   constructor(
     message: string,
     readonly value?: any,
     readonly errors?: string[],
-    CODE: string = "E2000"
+    CODE: string = ValidationError.CODE
   ) {
     super(CODE, message);
   }
 }
 
 export class EmailExistsError extends ValidationError {
+  static get CODE() {
+    return "E2001";
+  }
   constructor(email: string) {
     super(
       `A user with email ${email} already exists.`,
       email,
       undefined,
-      "E2001"
+      EmailExistsError.CODE
     );
   }
 }
