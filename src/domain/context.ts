d383a542f42711dc2f08f7aa96f3819f98f49d93
Mitica <canteadumitru@gmail.com>
2019-08-01 16:19:49 +0300

    simple context

--- src/domain/context.ts
+++ src/domain/context.ts
@@ -3,15 +3,12 @@ import { UseCaseManager } from "./usecases/usecase-manager";
 import { User } from "./entities/user/user";
 import { DomainConfig } from "./config";
 
-export interface DomainContext<TConfig extends DomainConfig = DomainConfig> {
-  config: Readonly<TConfig>;
+export interface DomainContext {
+  config: Readonly<DomainConfig>;
   repo: Readonly<RepositoryManager>;
   usecase: Readonly<UseCaseManager>;
 }
 
-export interface UserDomainContext<
-  TUser extends User = User,
-  TConfig extends DomainConfig = DomainConfig
-> extends DomainContext<TConfig> {
-  user: Readonly<TUser>;
+export interface UserDomainContext extends DomainContext {
+  user: Readonly<User>;
 }
